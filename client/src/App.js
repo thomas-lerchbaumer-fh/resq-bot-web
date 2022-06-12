2173df10540ec7b841b81ff4a869c91fefa41d2f
Markus Posch <markus.posch.mp1@wien.gv.at>
2022-06-12 17:02:09 +0200

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { Slider } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          
+        <Slider defaultValue={30} step={10} marks min={10} max={110} disabled />
+
       </header>
     </div>
   );
 }
+
+
 
 export default App;
