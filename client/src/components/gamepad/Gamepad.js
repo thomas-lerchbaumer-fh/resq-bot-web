1663561f0830f641aed21771c2e55db4e7d6c1bf
thomas-lerchbaumer-fh <71784753+thomas-lerchbaumer-fh@users.noreply.github.com>
2022-07-28 17:13:13 +0200

    *refactor

--- client/src/components/gamepad/Gamepad.js
+++ client/src/components/gamepad/Gamepad.js
@@ -1,88 +1,118 @@
-import React, { Fragment, useEffect, useContext, useState } from 'react';
+import React, { Fragment, useEffect, useContext, useState } from "react";
 import { useGamepads } from "react-gamepads";
-
-
-
-
+import { styled } from "@mui/material/styles";
+import { Grid } from "@mui/material";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
 
 const buttonLabels = [
-    "A",
-    "B",
-    "X",
-    "Y",
-    "L1",
-    "R1",
-    "L2",
-    "R2",
-    "Back",
-    "Start",
-    "L3",
-    "R3",
-    "UP",
-    "DOWN",
-    "LEFT",
-    "RIGHT",
-    "XBOX",
-]
+  "A",
+  "B",
+  "X",
+  "Y",
+  "L1",
+  "R1",
+  "L2",
+  "R2",
+  "Back",
+  "Start",
+  "L3",
+  "R3",
+  "UP",
+  "DOWN",
+  "LEFT",
+  "RIGHT",
+  "XBOX",
+];
 
-const axesLabels = [
-    "LX",
-    "LY",
-    "RX",
-    "RY",
-]
+const axesLabels = ["LX", "LY", "RX", "RY"];
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  height: 100,
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
+const Gamepad = () => {
+  const [gamepads, setGamepads] = useState([]);
+  useGamepads((_gamepads) => {
+    setGamepads(Object.values(_gamepads));
+  });
 
-const Gamepad = () =>{
-    const [gamepads, setGamepads] = useState([
+  if (gamepads.length > 0) {
+    const test = gamepads[0].buttons[4].pressed;
+    console.log(gamepads[0].axes[0]);
+  }
 
-    ])
-    useGamepads(_gamepads =>{
-        setGamepads(Object.values(_gamepads))
-        }
-    )
-
-    if(gamepads.length > 0){
-        const test = gamepads[0].buttons[4].pressed
-
-        console.log(gamepads[0].axes[0])
-
-
-    }
-
-
-    if(!gamepads) return 'no controller found'
-
+  if (gamepads.length === 0)
     return (
-        <Fragment>
-            <div className='gamepad'>
-               {gamepads.length && gamepads.map(pad =>{
-                   return(
-                       <div>
-                           <div>ID: {pad.id}</div>
-                           {pad.buttons.map((button, index) =>{
-                               return(
-                               <div>
-                                   <div><span>#{index +1} {button.value}</span></div>
-                               </div>
-                               )
-                           })}
-                           {pad.axes.map((axe,index)=>{
-                               return(
-                                   <div>
-                                       #{index +1}: {axe}
-                                   </div>
-                               )
-                           })}
-                       </div>
-                   )
+      <Fragment>
+        <Grid
+          item
+          xs={6}
+          lg={6}
+          sx={{ height: 300 }}
+          justifyContent="center"
+          alignContent="center"
+        >
+          <Item>
+            <Typography variant="h6">
+              Currently no controller is connected
+            </Typography>
+          </Item>
+        </Grid>
+        {/* <Grid item xs={5} lg={6}>
+                <Item><Typography variant="h6">Currently you are not connected to the robot</Typography></Item>
+                </Grid> */}
+      </Fragment>
+    );
+  return (
+    <Fragment>
+      <Grid
+        item
+        xs={6}
+        lg={6}
+        sx={{ height: 300 }}
+        justifyContent="center"
+        alignContent="center"
+      >
+        <Item>
+          <div className="gamepad">
+            {gamepads.length &&
+              gamepads.map((pad) => {
+                return (
+                  <div>
+                    <div>ID: {pad.id}</div>
+                    {pad.buttons.map((button, index) => {
+                      return (
+                        <div>
+                          <div>
+                            <span>
+                              #{index + 1} {button.value}
+                            </span>
+                          </div>
+                        </div>
+                      );
+                    })}
+                    {pad.axes.map((axe, index) => {
+                      return (
+                        <div>
+                          #{index + 1}: {axe}
+                        </div>
+                      );
+                    })}
+                  </div>
+                );
+              })}
+          </div>
+        </Item>
+      </Grid>
+    </Fragment>
+  );
+};
 
-               })}
-
-            </div>
-        </Fragment>
-    )
-}
-
-export default Gamepad
+export default Gamepad;
---
+++ client/src/components/gyro/Gyro.js
@@ -0,0 +1,150 @@
+import React, { Fragment, useEffect, useContext, useState } from "react";
+import MediaQuery from "react-responsive";
+import Button from "@mui/material/Button";
+import Slider from "@mui/material/Slider";
+import Divider from "@mui/material/Divider";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
+const sendLeftMotorControl = (event, newValue) => {
+  const requestOptions = {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ message: newValue }),
+  };
+};
+
+function getGyro() {
+  fetch("http://pi4.posch.ltd:3001/api")
+    .then((res) => res.json())
+    .then(
+      (result) => {
+        console.log(result);
+        document.getElementById("gyro").innerHTML = result["message"];
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      (error) => {}
+    );
+  /*
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+    // Note: the empty deps array [] means
+    // this useEffect will run once
+    // similar to componentDidMount()
+    useEffect(() => {
+      fetch("http://pi4.posch.ltd:3001/api")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            console.log(result);
+            document.getElementById("gyro").value = result; 
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+          }
+        )
+    }, [])
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              {item.name} {item.price}
+            </li>
+          ))}
+        </ul>
+      );
+    }*/
+}
+
+const Gyro = (event, newValue) => {
+  console.log(newValue);
+  return (
+    <Fragment>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid id="grid" container spacing={2}>
+          <Grid item xs={10.5}>
+            <Item id="gyro">Gyroskop X: Y: Z:</Item>
+          </Grid>
+        </Grid>
+        <Grid id="grid" container spacing={2}>
+          <Grid item xs={2}>
+            <Item>
+              Räder:
+              <Stack
+                sx={{ height: 335 }}
+                spacing={1}
+                divider={<Divider orientation="vertical" flexItem />}
+                alignItems="center"
+                justifyContent="center"
+                direction="row"
+              >
+                <Slider
+                  sx={{
+                    '& input[type="range"]': {
+                      WebkitAppearance: "slider-vertical",
+                    },
+                  }}
+                  marks
+                  step={10}
+                  orientation="vertical"
+                  defaultValue={30}
+                  aria-label="Temperature"
+                  valueLabelDisplay="auto"
+                  onChange={sendLeftMotorControl}
+                  // onKeyDown={preventHorizontalKeyboardNavigation}
+                />
+                <Slider
+                  sx={{
+                    '& input[type="range"]': {
+                      WebkitAppearance: "slider-vertical",
+                    },
+                  }}
+                  marks
+                  step={10}
+                  orientation="vertical"
+                  defaultValue={30}
+                  aria-label="Temperature"
+                  valueLabelDisplay="auto"
+                  // onKeyDown={preventHorizontalKeyboardNavigation}
+                />
+              </Stack>
+            </Item>
+          </Grid>
+          <Grid item xs={8.5}>
+            {/*     <Item><VideoFeed src="http://10.0.0.91:8083/stream/pattern/channel/0/hls/live/index.m3u8" /></Item> */}
+          </Grid>
+          <Grid item xs={10.5}>
+            <Item>
+              <Button variant="contained" onClick={getGyro}>
+                Hello World
+              </Button>
+            </Item>
+          </Grid>
+        </Grid>
+      </Box>
+    </Fragment>
+  );
+};
+
+export default Gyro;
