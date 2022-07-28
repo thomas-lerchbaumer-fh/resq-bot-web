3b4771d54a607909595b78be3dac51300eb89f53
thomas-lerchbaumer-fh <71784753+thomas-lerchbaumer-fh@users.noreply.github.com>
2022-07-28 17:12:45 +0200

    *refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,165 +1,47 @@
 //import logo from './logo.svg';
-import './App.css';
-import MediaQuery from 'react-responsive'
-import Button from '@mui/material/Button';
-import Slider from '@mui/material/Slider';
-import Divider from '@mui/material/Divider';
-import Stack from '@mui/material/Stack';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import VideoFeed from "./VideoFeed.tsx";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import * as React from 'react';
+import "./App.css";
+import { createTheme } from "@mui/material/styles";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+//import mui components here
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from "@emotion/react";
+import Container from '@mui/material/Container';
 
-function getGyro(){
-  
-  fetch("http://pi4.posch.ltd:3001/api")
-  .then(res => res.json())
-  .then(
-    (result) => {
-        console.log(result);
-        document.getElementById("gyro").innerHTML = result["message"]; 
+//import components here
+import Gamepad from "./components/gamepad/Gamepad";
+import Gyro from "./components/gyro/Gyro";
+import TopMenu from "./components/layouts/TopMenu/TopMenu";
+
+//import pages here
+import Home from "./pages/Home"
+
+const theme = createTheme({
+  palette: {
+    type: "light",
+    primary: {
+      main: "#181818",
     },
-    // Note: it's important to handle errors here
-    // instead of a catch() block so that we don't swallow
-    // exceptions from actual bugs in components.
-    (error) => {
-
-    }
-  )
-/*
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
-
-  // Note: the empty deps array [] means
-  // this useEffect will run once
-  // similar to componentDidMount()
-  useEffect(() => {
-    fetch("http://pi4.posch.ltd:3001/api")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          console.log(result);
-          document.getElementById("gyro").value = result; 
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-
-        }
-      )
-  }, [])
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <ul>
-        {items.map(item => (
-          <li key={item.id}>
-            {item.name} {item.price}
-          </li>
-        ))}
-      </ul>
-    );
-  }*/
-}
-
-
-
+    secondary: {
+      main: "#e00302",
+    },
+    background: {
+      default: "#252525",
+    },
+  },
+});
 
 function App() {
-  const sendLeftMotorControl = (event, newValue) => {
-    console.log(newValue);
-    const requestOptions = {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ message: newValue })
-  };
-  fetch('http://pi4.posch.ltd:3001/leftMotorControl', requestOptions)
-      .then(response => response.json())
-      .then(data => console.log(data));
-  };
   return (
-
     <div className="App">
-      <header className="App-header">
-        <Box sx={{ flexGrow: 1 }}>
-        <Grid id="grid" container spacing={2}>
-          <Grid item xs={10.5}>
-            <Item id="gyro">Gyroskop X:  Y:   Z:</Item>
-          </Grid>
-        </Grid>
-          <Grid id="grid" container spacing={2}>
-            <Grid item xs={2}>
-              <Item>
-                Räder:
-                <Stack sx={{ height: 335 }} 
-                spacing={1} 
-                divider={<Divider orientation="vertical" flexItem />}
-                alignItems="center"
-                justifyContent="center"
-                direction="row">
-                <Slider
-                    sx={{
-                      '& input[type="range"]': {
-                        WebkitAppearance: 'slider-vertical',
-                      },
-                    }}
-                    marks
-                    step={10}
-                    orientation="vertical"
-                    defaultValue={30}
-                    aria-label="Temperature"
-                    valueLabelDisplay="auto"
-                    onChange={sendLeftMotorControl}
-                  // onKeyDown={preventHorizontalKeyboardNavigation}
-                  />
-                  <Slider
-                    sx={{
-                      '& input[type="range"]': {
-                        WebkitAppearance: 'slider-vertical',
-                      },
-                    }}
-                    marks
-                    step={10}
-                    orientation="vertical"
-                    defaultValue={30}
-                    aria-label="Temperature"
-                    valueLabelDisplay="auto"
-                  // onKeyDown={preventHorizontalKeyboardNavigation}
-                  />
-                </Stack>
-              </Item>
-            </Grid >
-            <Grid item xs={8.5}>
-              <Item><VideoFeed src="http://10.0.0.91:8083/stream/pattern/channel/0/hls/live/index.m3u8" /></Item>
-            </Grid>
-            <Grid item xs={10.5}>
-              <Item><Button variant="contained" onClick={getGyro}>Hello World</Button></Item>
-            </Grid>
-          </Grid>
-        </Box>
+      <ThemeProvider theme={theme}>
+        <CssBaseline></CssBaseline>
+        <TopMenu></TopMenu>
 
-      </header>
+        <Home></Home>
+      </ThemeProvider>
     </div>
   );
-};
-
+}
 
 export default App;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,46 +1,39 @@
-import React, { useContext, useEffect, Fragment, useReducer, useState } from 'react'
-import {Container} from "@mui/material";
+import React, {
+  useContext,
+  useEffect,
+  Fragment,
+  useReducer,
+  useState,
+} from "react";
+import { Container } from "@mui/material";
 import Gamepad from "../components/gamepad/Gamepad";
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Gyro from "../components/gyro/Gyro";
 
+const Home = () => {
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
 
-const Home = () =>{
+  return (
+    <Fragment>
+      <Container maxWidth="lg">
+        <Box sx={{ flexGrow: 1, mt: 5 }}>
+          <Grid container justifyContent="center" spacing={2}>
+            <Gamepad></Gamepad>
+          </Grid>
+        </Box>
+        <Gyro></Gyro>
+      </Container>
+    </Fragment>
+  );
+};
 
-    const Item = styled(Paper)(({ theme }) => ({
-        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-        ...theme.typography.body2,
-        padding: theme.spacing(1),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    }));
-
-    return(
-        <Fragment>
-            <Container maxWidth={"xl"}>
-                <Grid container spacing={2}>
-                    <Grid item xs={6}>
-                        <Item>
-                            <Gamepad></Gamepad>
-                        </Item>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Item>
-                            <Gamepad></Gamepad>
-                        </Item>
-                    </Grid>
-                </Grid>
-
-
-            </Container>
-        </Fragment>
-
-    )
-
-
-}
-
-
-export default Home
+export default Home;
