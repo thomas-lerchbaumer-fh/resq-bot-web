6def9e99c4f0486aa708c760873dc704508f1164
thomas-lerchbaumer-fh <71784753+thomas-lerchbaumer-fh@users.noreply.github.com>
2022-07-29 19:20:02 +0200

    *gyro 3d + websockets integration

--- server/index.js
+++ server/index.js
@@ -2,8 +2,13 @@
 
 const express = require("express");
 const session = require('express-session');
+const http = require("http");
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
+
+const {Server} = require("socket.io");
+const { application } = require("express");
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,6 +18,48 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+const server = http.createServer(app)
+const io = new Server(server,{
+    cors:{
+        origin: "*"
+    }
+})
+
+const connections = [];
+io.sockets.on('connection', (socket) => {
+    connections.push(socket);
+    console.log(' %s sockets is connected', connections.length); // this is not printing
+
+    socket.on('disconnect', () => {
+        connections.splice(connections.indexOf(socket), 1);
+    });
+});
+
+const EventEmitter = require('events').EventEmitter;
+const myEmitter = new EventEmitter();
+
+myEmitter.on('updateGyro', function (jsonobj) {
+    // do something here like broadcasting data to everyone
+    // or you can check the connection with some logic and 
+    // only send to relevant user
+    connections.forEach(function(socket) {
+        socket.emit('gyroData', jsonobj);
+    });
+});
+
+app.post('/receiveGyro', function (req, res, next) {  
+    const data = req.body
+    console.log(req.body);
+
+    // emit your custom event with custom data
+    myEmitter.emit('updateGyro', data);
+
+    // send the response to avoid connection timeout
+    res.send({ok: true});
+});
+
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello bois" });
@@ -24,5 +71,10 @@ app.put("/leftMotorControl", (req,res) => {
 });
 
 app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
+
+
+server.listen(3002, () => {
     console.log(`Server listening on ${PORT}`);
 });
