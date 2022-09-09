8618d0735b19b4a1b5cc51c46afa0082a197dca2
thomas-lerchbaumer-fh <71784753+thomas-lerchbaumer-fh@users.noreply.github.com>
2022-09-09 15:10:55 +0200

    *adjusted Gamepad logic

--- client/src/components/gamepad/Gamepad.js
+++ client/src/components/gamepad/Gamepad.js
@@ -18,6 +18,11 @@ const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  return JSON.parse(tokenString)
+}
+
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
@@ -46,10 +51,7 @@ const sendControls = (button, data) => {
   'Accept':'application/json'}),
   body: JSON.stringify(req) // body data type must match "Content-Type" header
 })
-
-
 }
-
 
 const Gamepad = () => {
   const [gamepads, setGamepads] = useState([]);
@@ -83,7 +85,7 @@ const Gamepad = () => {
       gamepads[0].axes[1] < SENSITIVITY * -1 //up down
     ) {
 
-      sendControls("x", gamepads[0].axes);
+      sendControls("ls", gamepads[0].axes);
     }
 
     //right analog stick
@@ -93,22 +95,29 @@ const Gamepad = () => {
       gamepads[0].axes[2] < SENSITIVITY * -1 || //left right
       gamepads[0].axes[3] < SENSITIVITY * -1 //up down
     ) {
-      console.log(gamepads[0].axes);
+     
+      sendControls("rs", gamepads[0].axes);
     }
 
     //right shoulder bottom (r2)
-    if (gamepads[0].buttons[7].pressed === true)
+    if (gamepads[0].buttons[7].pressed === true){
+      sendControls("r2",gamepads[0].buttons[7]);
       console.log(gamepads[0].buttons[7]);
+    }
+      
 
     //left shoulder bottom (l2)
-    if (gamepads[0].buttons[6].pressed === true)
+    if (gamepads[0].buttons[6].pressed === true){
+      sendControls("r2",gamepads[0].buttons[6]);
       console.log(gamepads[0].buttons[6]);
+    }
+     
   }
 
   if (gamepads.length > 0) {
     const test = gamepads[0].buttons[4].pressed;
   }
-  if (gamepads.length === 0)
+  if (gamepads.length === 0){
     return (
       <Fragment>
         <Grid
@@ -137,37 +146,71 @@ const Gamepad = () => {
           </Item>
         </Grid>
       </Fragment>
-    );
-  return (
-    <Fragment>
-      {
-        <Grid item xs={12} lg={6} justifyContent="center" alignContent="center">
-          <Item
-            sx={{
-              flexDirection: "column",
-              justifyContent: "space-evenly",
-            }}
-          >
-            <Typography variant="h6" color="success.main">
-              Controller connected{" "}
-              <TaskAltIcon style={{ verticalAlign: "middle" }} />{" "}
+    );}
+    else if(gamepads.length > 0 && getToken() !== "yes"){
+      return(
+        <Fragment>
+             <Grid
+          item
+          xs={12}
+          md={12}
+          lg={6}
+          sx={{ height: "100%" }}
+          justifyContent="center"
+          alignContent="center"
+          style={{
+            justifyContent: "center",
+          }}
+        >
+          <Item>
+            <Typography variant="h6">
+             Login to control the robot
             </Typography>
-            <VisualizePad></VisualizePad>
+            <Link to="/loginpage">
+            <Tooltip title="Login">
+              <IconButton edge="end">
+                <InfoIcon fontSize="small" tooltip="Click to login"></InfoIcon>
+              </IconButton>
+            </Tooltip>
+            </Link>
           </Item>
+
         </Grid>
-      }
-      <Snackbar
-        open={connected}
-        autoHideDuration={4000}
-        onClose={handleClose}
-        severity="success"
-      >
-        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
-          Controller is connected now
-        </Alert>
-      </Snackbar>
-    </Fragment>
-  );
+        </Fragment>
+      )
+    }else if (gamepads.length > 0 && getToken() === "yes"){
+      return (
+        <Fragment>
+          {
+            <Grid item xs={12} lg={6} justifyContent="center" alignContent="center">
+              <Item
+                sx={{
+                  flexDirection: "column",
+                  justifyContent: "space-evenly",
+                }}
+              >
+                <Typography variant="h6" color="success.main">
+                  Controller connected{" "}
+                  <TaskAltIcon style={{ verticalAlign: "middle" }} />{" "}
+                </Typography>
+                <VisualizePad></VisualizePad>
+              </Item>
+            </Grid>
+          }
+          <Snackbar
+            open={connected}
+            autoHideDuration={4000}
+            onClose={handleClose}
+            severity="success"
+          >
+            <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+              Controller is connected now
+            </Alert>
+          </Snackbar>
+        </Fragment>
+      );
+    }
+
 };
 
 export default Gamepad;
